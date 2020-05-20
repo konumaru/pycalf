2e49b833a5d97043217f05cbdccc45a3424ace7c
konumaru <konumaru1022@gmail.com>
2020-05-20 22:06:41 +0900

    [update] define and edit BaseModel.

--- pycalf/PropensityScore.py
+++ pycalf/PropensityScore.py
@@ -1,13 +1,66 @@
 import numpy as np
 import pandas as pd
 
+from sklearn import metrics
+from sklearn.metrics import roc_curve, auc
+from sklearn.metrics import roc_auc_score
 
-class IPW:
-    def __init__(self, learner, clip_bounds=(1e-3, 1 - 1e-3)):
+import matplotlib.pyplot as plt
+plt.style.use('seaborn')
+
+
+class BaseModel:
+    def __init__(self, learner):
         self.learner = learner
-        self.clip_bounds = clip_bounds
         self.ps = None
         self.weight = None
+
+    def acu(self, treatment):
+        return roc_auc_score(treatment, self.ps)
+
+    def plot_roc_curve(self, treatment, figsize=(7, 6)):
+        fpr, tpr, thresholds = metrics.roc_curve(treatment, self.ps)
+        auc = metrics.auc(fpr, tpr)
+
+        plt.figure(figsize=figsize)
+        plt.plot(fpr, tpr, color='darkorange', lw=2, label='ROC curve (area = %0.2f)' % auc)
+        plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.05])
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.title('ROC Curve')
+        plt.legend(loc="lower right")
+        plt.show()
+
+    def plot_propensity_score(self, treatment, figsize=(12, 6)):
+        plt.figure(figsize=figsize)
+        plt.title('Propensity Score Distoribution.')
+        plt.xlabel('Propensity Score')
+        plt.ylabel('The Number of Data')
+        plt.hist(
+            self.ps[treatment == 0],
+            bins=np.linspace(0, 1, 100, endpoint=False),
+            rwidth=0.4,
+            align='left',
+            color='tab:blue'
+        )
+        plt.hist(
+            self.ps[treatment == 1],
+            bins=np.linspace(0, 1, 100, endpoint=False),
+            rwidth=0.4,
+            align='mid',
+            color='tab:orange'
+        )
+        plt.show()
+        pass
+
+
+class IPW(BaseModel):
+
+    def __init__(self, learner, clip_bounds=(1e-3, 1 - 1e-3)):
+        self.clip_bounds = clip_bounds
+        super().__init__(learner)
 
     def fit(self, X, treatment):
         self.learner.fit(X, treatment)
