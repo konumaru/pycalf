f8cf3a5c0ff3c826ce63112553548be4581dccb1
konumaru <konumaru1022@gmail.com>
2020-10-28 11:30:19 +0900

    [update] add Matching method.

--- pycalf/propensity_score.py
+++ pycalf/propensity_score.py
@@ -2,10 +2,63 @@ import copy
 import numpy as np
 import pandas as pd
 
+from sklearn.neighbors import NearestNeighbors
+
 
 class Matching():
-    def __init__(self):
-        pass
+    def __init__(self, learner, eps=1e-2):
+        self.learner = learner
+        self.p_score = None
+        self.eps = 1e-2
+
+    def fit(self, X, treatment, y, clip=1e-15):
+        self.learner.fit(X, treatment)
+        assert 0 <= clip < 1, 'clip must be 0 to 1.'
+        self.p_score = np.clip(self.learner.predict_proba(X)[:, 1], clip, 1 - clip)
+
+    def get_score(self):
+        return self.p_score
+
+    def get_weight(self, treatment, mode='ate'):
+        self._check_mode(mode)
+        if mode == 'raw':
+            return np.ones(treatment.shape[0])
+        elif mode == 'ate':
+            return self._get_matche_weight(treatment, self.p_score)
+
+    def _check_mode(self, mode):
+        mode_list = ['raw', 'ate', 'att', 'atu']
+        assert mode in mode_list, 'mode must be string and it is raw, ate, att or atu.'
+
+    def _get_matche_weight(self, treatment, score):
+        neigh = NearestNeighbors(n_neighbors=5, metric='manhattan')
+        neigh.fit(score[~treatment].reshape(-1, 1))
+        neigh_dist, neigh_idx = neigh.kneighbors(
+            score[treatment].reshape(-1, 1), 1, return_distance=True)
+        neigh_idx = neigh_idx[neigh_dist < self.eps]
+
+        treat_idx = np.where(treatment)[0]
+        control_idx = np.where(~treatment)[0]
+        treat_idx = treat_idx
+        control_idx = control_idx[neigh_idx.flatten()]
+
+        smpl_idx = np.concatenate((control_idx, treat_idx), axis=0)
+        idx, counts = np.unique(smpl_idx, return_counts=True)
+        weights = np.zeros(treatment.shape[0])
+        for i, c in zip(idx, counts):
+            weights[i] = c
+        return weights
+
+    def estimate_effect(self, treatment, y, mode='ate'):
+        self._check_mode(mode)
+        weight = self.get_weight(treatment, mode=mode)
+        return self._estimate_outcomes(treatment, y, weight)
+
+    def _estimate_outcomes(self, treatment, y, weight):
+        avg_y_control = np.average(y[~treatment], axis=0, weights=weight[~treatment])
+        avg_y_treat = np.average(y[treatment], axis=0, weights=weight[treatment])
+        effect_size = avg_y_treat - avg_y_control
+        return (avg_y_control, avg_y_treat, effect_size)
 
 
 class IPW():
