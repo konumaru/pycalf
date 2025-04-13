37e40504d9daf9edd6236ecac6a8d8edb37b9552
konumaru <konumaru1022@gmail.com>
2025-04-14 03:16:36 +0900

    fix: update copyright year and add autodoc mock imports

--- docs/conf.py
+++ docs/conf.py
@@ -3,12 +3,16 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(".."))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "pycalf"
-copyright = "2023, konumaru"
+copyright = "2025, konumaru"
 author = "konumaru"
 release = "0.1"
 
@@ -21,6 +25,9 @@ extensions = [
     "sphinx.ext.todo",
     "sphinx.ext.autodoc",
 ]
+
+# モックする外部依存パッケージ
+autodoc_mock_imports = ["numpy", "matplotlib", "pandas", "sklearn", "statsmodels"]
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
